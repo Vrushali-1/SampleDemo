d6808a285231e40d2a438393630debe4493b6586
Vrushali-1 <vrushalipatil773@gmail.com>
2020-03-07 17:28:04 +0530

    helloworld added

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+public class HelloWorld{
+
+  private String str=null;
+public static void main(String args[]){
+   
+String str1="abc";
+String str2="pqr";
+
+if(str1==str2){
+   System.out.println("Both are equal");
+}
+
+System.out.println("Vrushali");
+//This is a comment
+
+}
+
+
+}
